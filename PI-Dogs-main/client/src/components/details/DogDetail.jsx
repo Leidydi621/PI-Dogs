623d8422d593fbef507afccfc19255507b9e6fe3
leidydi621 <leidydi621@gmail.com>
2022-03-15 20:26:11 -0500

    FINAL STYLES

--- PI-Dogs-main/client/src/components/details/DogDetail.jsx
+++ PI-Dogs-main/client/src/components/details/DogDetail.jsx
@@ -4,6 +4,8 @@ import { useEffect } from 'react';
 import { getDogById } from '../../actions';
 import {Link} from 'react-router-dom';
 
+import style from './DogDetail.module.css';
+
 export default function Detail (props){
 
     const myDog = useSelector((state) => state.detail)
@@ -16,23 +18,30 @@ export default function Detail (props){
 
 
     return (
-        <div>
+        <div className={style.container}>
             {
                 myDog.length>0 ?
                 <div>
-                    <h1>{myDog[0].name}</h1>
-                    <img src={myDog[0].image } alt= "img not found" width= "300px" height= "300px" />
-                    <h2>Weight: {myDog[0].weight}</h2>
-                    <h2>Height: {myDog[0].height}</h2>
-                    <h2>Life span: {myDog[0].life_span}</h2>
-                    <h4>Temperaments: {!myDog[0].createdInDb? myDog[0].temperament : myDog[0].temperaments.map(el => el.name + (', '))}</h4>
+                    <div>
+                        <img className={style.img} src={myDog[0].image} alt= "img not found" width= "300px" height= "300px" />
+                    </div>
+                    <div className={style.containerCard}>
+                    <div className={style.card}>
+                        <h1>{myDog[0].name}</h1>
+                        <h2>Weight: {myDog[0].weight}</h2>
+                        <h2>Height: {myDog[0].height}</h2>
+                        <h2>Life span: {myDog[0].life_span}</h2>
+                        <h4>Temperaments: {!myDog[0].createdInDb? myDog[0].temperament : myDog[0].temperaments.map(el => el.name + (', '))}</h4>
+                        <Link to='/home'>
+                          <button className={style.btn}>Go Back</button>
+                        </Link>
+
+                    </div>
+                    </div>
 
                 </div> : <p>Loading...</p>
             }
 
-            <Link to='/home'>
-                <button>Go Back</button>
-            </Link>
         </div>
     )
 
